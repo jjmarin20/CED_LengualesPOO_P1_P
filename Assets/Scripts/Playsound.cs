15323bda1dfebd73eaf62363ddca11d081526471
KarenAlexa99 <55495447+KarenAlexa99@users.noreply.github.com>
2019-09-30 09:44:41 -0500

    sonido

---
+++ Assets/Scripts/Playsound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playsound : MonoBehaviour
+{
+    
+    public AudioClip disparoplayer;
+
+    AudioSource fuenteaudio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fuenteaudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            fuenteaudio.clip = disparoplayer;
+            fuenteaudio.Play();
+        }
+    }
+}
