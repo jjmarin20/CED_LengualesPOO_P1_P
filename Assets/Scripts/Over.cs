b3026669d8f17350a855096a98d9ea2f48d2185d
jjmarin20 <juanjosemarin2000@gmail.com>
2019-09-30 09:29:43 -0500

    Game over scene
    escena del game over

---
+++ Assets/Scripts/Over.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Over : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void RESTART()
+    {
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("has quit game");
+        Application.Quit();
+    }
+}
